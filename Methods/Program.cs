2cc6e89863ed5ee31d1e61531510d3e86438dd6b
KaidenT <89858394+alilgae@users.noreply.github.com>
2024-10-03 20:17:07 -0400

    Initial setup

---
+++ Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Jin Yi & Kaiden Terrana
+ * IGME 201
+ * Due Oct 13, 2024
+ */
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
